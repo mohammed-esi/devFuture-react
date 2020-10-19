de20b3e0fa823cba296143f68c207bf8f1db2217
Mohammed Elamine <zighed.mohammed.alamine@gmail.com>
2020-10-19 16:52:44 +0200

    add experience and education components

---
+++ client/src/components/dashboard/Education.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export default function Education() {
+  return (
+    <div className='card px-2'>
+      <div className='card-body'>
+        <h3 className='my-4'>Education Credentials</h3>
+        <div className='my-3'>
+          <table className='table mr-4'>
+            <thead>
+              <tr>
+                <th scope='col'>Scool</th>
+                <th scope='col'>Degree</th>
+                <th scope='col'>Years</th>
+                <th scope='col' />
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>Otto</td>
+                <td>
+                  <button className='btn btn-card btn-lg'>
+                    <i className='fas fa-trash mr-2' />
+                    Delete
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>Thornton</td>
+                <td>
+                  <button className='btn btn-card btn-lg'>
+                    <i className='fas fa-trash mr-2' />
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+export default function Experience() {
+  return (
+    <div className='card px-2'>
+      <div className='card-body'>
+        <h3 className='my-4'>Experience Credentials</h3>
+        <div className='my-3'>
+          <table className='table mr-4'>
+            <thead>
+              <tr>
+                <th scope='col'>Company</th>
+                <th scope='col'>Title</th>
+                <th scope='col'>Years</th>
+                <th scope='col' />
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>Otto</td>
+                <td>
+                  <button className='btn btn-card btn-lg'>
+                    <i className='fas fa-trash mr-2' />
+                    Delete
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>Otto</td>
+                <td>
+                  <button className='btn btn-card btn-lg'>
+                    <i className='fas fa-trash mr-2' />
+                    Delete
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>Larry</td>
+                <td>Otto</td>
+                <td>the Bird</td>
+                <td>
+                  <button className='btn btn-card btn-lg'>
+                    <i className='fas fa-trash mr-2' />
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  )
+}
