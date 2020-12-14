19731f12d0f2348cd42488ed0ab8cd0c72663b2c
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2020-12-14 11:34:12 +0100

    edit get repos in github

--- routes/profiles.js
+++ routes/profiles.js
@@ -350,14 +350,16 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 router.get('/github/:username', async (req, res) => {
   try {
     const uri = encodeURI(
-      `https://api.github.com/users/${
-        req.params.username
-      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
-        'githubClientId'
-      )}&client_secret=${config.get('githubSecret')}`
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+      // `https://api.github.com/users/${
+      //   req.params.username
+      // }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+      //   'githubClientId'
+      // )}&client_secret=${config.get('githubSecret')}`
     );
     const headers = {
       'user-agent': 'node.js',
+      Authorization: `token ${config.get('githubToken')}`
     };
 
     const gitHubResponse = await axios.get(uri, { headers });
