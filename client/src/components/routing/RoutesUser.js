c4e28fc83b841063ad7166ce15b955f013444f5d
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2020-12-15 13:10:25 +0100

    add the profiles in routes

--- client/src/components/routing/RoutesUser.js
+++ client/src/components/routing/RoutesUser.js
@@ -6,6 +6,7 @@ import CreateProfileForm from '../profileForms/CreateProfileForm'
 import AddEducation from '../profileForms/AddEducation'
 import AddExpereince from '../profileForms/AddExperience'
 import Profile from '../profile/Profile'
+import Profiles from '../profiles/Profiles'
 import Navbar from '../layout/Navbar'
 import Footer from '../layout/Footer'
 
@@ -15,6 +16,7 @@ export default function RoutesUser() {
       <Navbar />
       <Switch>
         <Route exact path='/profile/:id' component={Profile} />
+        <Route exact path='/profiles' component={Profiles} />
         <PrivateRoute exact path='/edit-profile' component={EditProfileForm}  />
         <PrivateRoute exact path='/create-profile' component={CreateProfileForm}  />
         <PrivateRoute exact path='/edit-education' component={AddEducation}  />
