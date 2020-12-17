2ec1f0e44e4de470e9d47d7c89b11d55ef788728
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2020-12-17 12:47:24 +0100

    add service in routes

--- client/src/components/routing/RoutesUser.js
+++ client/src/components/routing/RoutesUser.js
@@ -7,6 +7,8 @@ import AddEducation from '../profileForms/AddEducation'
 import AddExpereince from '../profileForms/AddExperience'
 import Profile from '../profile/Profile'
 import Profiles from '../profiles/Profiles'
+import Services from '../services/Services'
+import CreateService from '../services/ServiceForm'
 import Navbar from '../layout/Navbar'
 import Footer from '../layout/Footer'
 
@@ -17,6 +19,8 @@ export default function RoutesUser() {
       <Switch>
         <Route exact path='/profile/:id' component={Profile} />
         <Route exact path='/profiles' component={Profiles} />
+        <Route exact path='/services' component={Services} />
+        <PrivateRoute exact path='/create-service' component={CreateService}  />
         <PrivateRoute exact path='/edit-profile' component={EditProfileForm}  />
         <PrivateRoute exact path='/create-profile' component={CreateProfileForm}  />
         <PrivateRoute exact path='/edit-education' component={AddEducation}  />
